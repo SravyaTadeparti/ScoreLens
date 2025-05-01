0e8e11b1b1f98cf1da5ac697310a81a06897630e
Sriya Surisetty <148708651+SriyaSurisetty@users.noreply.github.com>
2025-05-01 16:38:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './LandingPage';
-import Dashboard_p from './Dashboard_p';
-import Dashboard_s from './Dashboard_s';
-import LoginPage from './LoginPage';
+import Sidebar from './components/Sidebar';
+import Marksheets from './components/Marksheets';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/dashboard_p" element={<Dashboard_p />} />
-        <Route path="/dashboard_s" element={<Dashboard_s />} />
-      </Routes>
+      <div className="flex">
+        <Sidebar />
+        <div className="flex-grow p-6">
+          <Routes>
+            <Route path="/marksheets" element={<Marksheets />} />
+            {/* You can add other routes later like individual-scores, ranks, etc. */}
+          </Routes>
+        </div>
+      </div>
     </Router>
   );
 }
